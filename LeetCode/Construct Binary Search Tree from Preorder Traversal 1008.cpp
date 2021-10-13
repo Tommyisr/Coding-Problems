0e00630a8ae2c399ec383f7e27911e217243eec4
Tommy Bogdanovich <37695491+Tommyisr@users.noreply.github.com>
2021-10-13 19:39:40 +0300

    Create Construct Binary Search Tree from Preorder Traversal 1008.cpp

---
+++ LeetCode/Construct Binary Search Tree from Preorder Traversal 1008.cpp
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* rec(vector<int>& array, int* index, int max){
+        
+        
+        
+        
+        if(*index == array.size() || array[*index] > max) return NULL;
+        
+        TreeNode* t = new TreeNode(array[*index]);
+        
+        *index = *index+1;
+        t->left = rec(array,index,t->val);
+        t->right = rec(array,index,max);
+        
+        
+        return t;
+    }
+    
+    TreeNode* bstFromPreorder(vector<int>& preorder) {
+        
+        
+        int index = 0;
+        return rec(preorder,&index, INT_MAX);;
+        
+    }
+};
