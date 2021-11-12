ba95807eff7c5166c89eabd6e18ad3695609daee
Tommy Bogdanovich <37695491+Tommyisr@users.noreply.github.com>
2021-11-12 02:16:04 +0200

    Create A. Watermelon.cpp

---
+++ CodeForces/A. Watermelon.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+ 
+ 
+int main(){
+    
+    int arbuz;
+    cin >> arbuz;
+    
+    if(arbuz!= 2 && arbuz%2==0) cout << "YES" << endl;
+    else cout << "NO" << endl;
+    
+    
+    return 0;
+}
